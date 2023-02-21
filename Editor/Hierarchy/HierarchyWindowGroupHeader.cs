b5e99f57d36e087798eaa2dc2d04a2419e8541e1
Cory <Cory.McAdam.96@gmail.com>
2023-02-21 19:03:25 +0000

    Added hierarchy window header

---
+++ Editor/Hierarchy/HierarchyWindowGroupHeader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class HierarchyWindowGroupHeader
+{
+    static HierarchyWindowGroupHeader()
+    {
+        EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;
+    }
+
+    static void HierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
+    {
+        var gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+
+        if (gameObject == null)
+            return;
+
+        if (!gameObject.CompareTag("EditorOnly"))
+            return;
+
+        if (gameObject.name.StartsWith("---", System.StringComparison.Ordinal))
+        {
+            EditorGUI.DrawRect(selectionRect, Color.gray);
+            EditorGUI.DropShadowLabel(selectionRect, gameObject.name.Replace("---", "").ToUpperInvariant());
+        }
+    }
+}
